2743eb4306db572389a4df1365f896af1c1c43b3
Shyam <syam.sp0965@gmail.com>
2015-11-18 17:30:13 +0530

    Removed duplicate factory

--- js/app.js
+++ js/app.js
@@ -10,20 +10,7 @@ angular.module('WealthyApp', ['ui.router', 'ngResource', 'FetchApi'])
 			$scope.FetchAddresses(560102);
 
 	}])
-	.factory('Address',function($resource){
-		return $resource('https://data.gov.in/api/datastore/resource.json?resource_id=6176ee09-3d56-4a3b-8115-21841576b2f6&api-key=f7bea1fcf1a8c41d6f6e372c876308e7&',{
-			'filters[pincode]' : '@pincode'
-		},
-		{
-			getAddressDetails : {
-				method : 'GET',
-				isArray : false,
-				transformResponse: function(data, header) {
-			          return (angular.fromJson(data));
-			        }
-			}
-		})
-	})
+	
 	.config(function($stateProvider, $urlRouterProvider) {
 	    
 	    $urlRouterProvider.otherwise('/');
--- js/services/apiService.js
+++ js/services/apiService.js
@@ -1,7 +1,7 @@
 angular.module('FetchApi',['ngResource'])
 	.factory('Address',function($resource){
 		return $resource('https://data.gov.in/api/datastore/resource.json',{
-			'filters[pincode]' : '@pincode',
+			'filters[pincode]': '',
 			'resource_id' : '6176ee09-3d56-4a3b-8115-21841576b2f6',
 			'api-key' : 'f7bea1fcf1a8c41d6f6e372c876308e7'
 		},
